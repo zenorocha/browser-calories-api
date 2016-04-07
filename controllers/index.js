6ff80824ae102eacfbe66ffa213d5d23d4371ee3
Zeno Rocha <hi@zenorocha.com>
2016-04-07 16:46:28 -0700

    Standardize indentation

--- controllers/index.js
+++ controllers/index.js
@@ -4,52 +4,52 @@ var bytes  = require('byte-size');
 var phantomas = require('phantomas');
 
 function controller(request, reply) {
-	var url = decodeURIComponent(request.query.url);
+  var url = decodeURIComponent(request.query.url);
 
-	phantomas(url, function(error, response) {
-		if (error) {
-			return reply(boom.create(error.code, error.message));
-		}
+  phantomas(url, function(error, response) {
+    if (error) {
+      return reply(boom.create(error.code, error.message));
+    }
 
-		var site = {
-			html  : response.metrics.htmlSize,
-			css   : response.metrics.cssSize,
-			image : response.metrics.imageSize,
-			js    : response.metrics.jsSize,
-			other : response.metrics.otherSize,
-			total : response.metrics.contentLength
-		};
+    var site = {
+      html  : response.metrics.htmlSize,
+      css   : response.metrics.cssSize,
+      image : response.metrics.imageSize,
+      js    : response.metrics.jsSize,
+      other : response.metrics.otherSize,
+      total : response.metrics.contentLength
+    };
 
-		return reply({
-			siteBytes: controller.toBytes(site),
-			budgetBytes: controller.toBytes(budget),
-			dailyPercentage: controller.toPercentage(site, budget)
-		});
-	});
+    return reply({
+      siteBytes: controller.toBytes(site),
+      budgetBytes: controller.toBytes(budget),
+      dailyPercentage: controller.toPercentage(site, budget)
+    });
+  });
 }
 
 controller.toBytes = function(data) {
-	var obj = {};
+  var obj = {};
 
-	for (item in data) {
-		if (data.hasOwnProperty(item)) {
-			obj[item] = bytes(data[item], { precision: 1 });
-		}
-	}
+  for (item in data) {
+    if (data.hasOwnProperty(item)) {
+      obj[item] = bytes(data[item], { precision: 1 });
+    }
+  }
 
-	return obj;
+  return obj;
 }
 
 controller.toPercentage = function(a, b) {
-	var obj = {};
+  var obj = {};
 
-	for (item in a) {
-		if (a.hasOwnProperty(item) && b.hasOwnProperty(item)) {
-			obj[item] = Math.round((a[item] / b[item]) * 100) + '%';
-		}
-	}
+  for (item in a) {
+    if (a.hasOwnProperty(item) && b.hasOwnProperty(item)) {
+      obj[item] = Math.round((a[item] / b[item]) * 100) + '%';
+    }
+  }
 
-	return obj;
+  return obj;
 }
 
 module.exports = controller;
--- server.js
+++ server.js
@@ -5,40 +5,40 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 
 server.connection({
-    port: process.env.PORT || 4000
+  port: process.env.PORT || 4000
 });
 
 server.route([
-    {
-        method: 'GET',
-        path: '/',
-        handler: require('./controllers/index')
-    }
+  {
+    method: 'GET',
+    path: '/',
+    handler: require('./controllers/index')
+  }
 ]);
 
 // -- Start --------------------------------------------------------------------
 
 server.register({
-    register: require('good'),
-    options: {
-        reporters: [{
-            reporter: require('good-console'),
-            events: {
-                log: '*',
-                error: '*'
-            }
-        }]
-    }
+  register: require('good'),
+  options: {
+    reporters: [{
+      reporter: require('good-console'),
+      events: {
+        log: '*',
+        error: '*'
+      }
+    }]
+  }
 }, function (err) {
-    if (err) {
-        throw err;
-    }
+  if (err) {
+    throw err;
+  }
 
-    if (!module.parent) {
-        server.start(function () {
-            server.log('info', 'Server running at: ' + server.info.uri);
-        });
-    }
+  if (!module.parent) {
+    server.start(function () {
+      server.log('info', 'Server running at: ' + server.info.uri);
+    });
+  }
 });
 
 module.exports = server;
