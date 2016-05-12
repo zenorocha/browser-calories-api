6f216a1c91a53f486fb1de177ee9081509b6e26a
Zeno Rocha <hi@zenorocha.com>
2016-05-12 15:25:16 -0700

    Improves error handling

--- controllers/index.js
+++ controllers/index.js
@@ -1,56 +1,47 @@
 var boom = require('boom');
-var psi  = require('psi');
+var superagent = require('superagent');
 
 function controller(request, reply) {
-  var url = decodeURIComponent(request.query.url);
-  var options = controller.getOptions();
-
-  psi(url, options).then(function(data) {
-    var result = {};
-
-    result = controller.toInt(data.pageStats);
-    result.total = controller.toTotal(result);
-
-    return reply(result);
-  });
+  superagent
+    .get(controller.getUrl(request.query.url))
+    .end(function(error, response) {
+      if (error || !response.ok) {
+        return reply(controller.getError(response.error));
+      } else {
+        return reply(controller.getResult(response.body.pageStats));
+      }
+    });
 }
 
-controller.getOptions = function() {
-  var options;
+controller.getUrl = function(url) {
+  var decodedUrl = decodeURIComponent(url);
+  var endpoint = 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=' + decodedUrl;
 
   if (process.env.API_KEY) {
-    options = {
-      key: process.env.API_KEY
-    };
-  } else {
-    options = {
-      nokey: true
-    };
+    endpoint += '&key=' + process.env.API_KEY;
   }
 
-  return options;
-};
+  return endpoint;
+}
 
-controller.toInt = function(data) {
-  return {
+controller.getError = function(data) {
+  var message = JSON.parse(data.text).error.errors[0].message;
+
+  return boom.create(data.status, message);
+}
+
+controller.getResult = function(data) {
+  var result = {
     html  : parseInt(data.htmlResponseBytes, 10) || 0,
     css   : parseInt(data.cssResponseBytes, 10) || 0,
     image : parseInt(data.imageResponseBytes, 10) || 0,
     js    : parseInt(data.javascriptResponseBytes, 10) || 0,
     other : parseInt(data.otherResponseBytes, 10) || 0
   };
+
+  result.total = result.html + result.image + result.css + result.js + result.other;
+
+  return result;
 }
-
-controller.toTotal = function(data) {
-  var total = 0;
-
-  for (item in data) {
-    if (data.hasOwnProperty(item)) {
-      total += data[item];
-    }
-  }
-
-  return total;
-};
 
 module.exports = controller;
